4e82853752c0ff7cc1fa04289b07651ab08c273b
juliozulu2008 <juliozulu2008@gmail.com>
2022-09-20 05:03:53 -0300

    begin of deploy

--- backend/routes/portifolio.js
+++ backend/routes/portifolio.js
@@ -2,27 +2,7 @@ const router = require('express').Router();
 
 const Portifolio = require('../models/Portifolio');
 
-router.get('/', async (req, res) =>{
-    try {
-        const portfolio = await Portifolio.find()
-    
-        res.json({
-            succes:true,
-            data:portfolio
-        })
-
-    } catch (error){
-        res.json({
-            succes:false,
-            message: error
-        })
-    }
-
-    res.json({
-        'succes':true
-    });
-});
-
+//CREATE
 router.post('/', async(req, res) =>{
     const portifolio = new Portifolio({
         title: req.body.title,
@@ -43,5 +23,84 @@ router.post('/', async(req, res) =>{
         
     }
 }); // gravando conteudo no banco de dados
+
+
+//READ
+router.get('/', async (req, res) =>{
+    try {
+        const portfolio = await Portifolio.find()
+    
+        res.json({
+            succes:true,
+            data:portfolio
+        })
+
+    } catch (error){
+        res.json({
+            succes:false,
+            message: error
+        })
+    }
+});
+
+router.get('/:slug', async(req, res) => {
+    try {
+        const portfolio = await Portifolio.findOne({
+            slug: req.params.slug
+        })
+        res.json({
+            succes:true,
+            data:portfolio
+        })
+    } catch (error) {
+        res.json({
+          succes:false,
+          message: error  
+        })
+    }
+})
+
+//UPDATE
+
+router.patch('/:slug', async(req, res) =>{
+    try {
+        const updatePortifolio = await Portifolio.updateOne({
+            slug: req.params.slug,
+        },
+        {
+        title: req.body.title,
+        description: req.body.description
+        })
+
+        res.json({
+            succes:true,
+            updated: updatePortifolio.modifiedCount
+        })
+    } catch (error) {
+        res.json({
+            succes:false,
+            message: error
+        })
+    }
+} )
+
+//DELETE
+
+router.delete('/:slug', async(req, res) =>{
+    try {
+        const deletedPortifolio = await Portifolio.deleteOne({
+        slug: req.params.slug,
+        })
+        res.json({
+            succes:true,
+            deleted: deletedPortifolio.deletedCount
+        })
+    } catch (error) {
+        res.json({
+            succes:false,
+            message: error
+        })
+    }
+})
 
 module.exports = router
