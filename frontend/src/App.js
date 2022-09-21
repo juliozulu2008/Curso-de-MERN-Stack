3176fdae36758d4b64350d6836eeb5ce60deec17
juliozulu2008 <juliozulu2008@gmail.com>
2022-09-20 22:49:45 -0300

    styles

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Global Style
+import GlobalStyle from './components/GlobalStyle';
+
+//Componentes
+import AboutUs from './pages/AboutUs';
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle/>
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
