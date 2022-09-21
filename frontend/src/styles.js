4e9d0d4995a23a04663e1f9a3f7c816227070f0d
juliozulu2008 <juliozulu2008@gmail.com>
2022-09-20 22:49:28 -0300

    start a front end development

---
+++ frontend/src/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+export const Description = styled.div`
+
+
+
+`;
